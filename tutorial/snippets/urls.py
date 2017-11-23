546373e0dde6051218b5b7b7059ff2bb26264635
lgants <lgants@gmail.com>
2017-11-22 23:49:34 -0600

    updated request/responses

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^snippets/(?P<pk>[0-9]+)$', views.snippet_detail),
 ]
+
+# Added format suffixes to give URLs that explicitly refer to a given format - allows API to handle URLs such as http://example.com/api/items/4.json.
+urlpatterns = format_suffix_patterns(urlpatterns)
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,3 +1,53 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+# Deprecating initial views removed explicitly tying requests/responses to json content type; request.data can handle incoming json requests and other formats
+@api_view(['GET', 'POST'])
+def snippet_list(request, format=None):
+    # List all code snippets, or create a new snippet
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk, format=None):
+    # Retrieve, update or delete a code snippet
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = SnippetSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+"""
+# Deprecated when switching to named status codes and api_view decorator
+
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -6,13 +56,11 @@ from rest_framework.parsers import JSONParser
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
-# NOTE need to use csrf_exempt decorator to POST to this view from clients without a CSRF token
+# Need to use csrf_exempt decorator to POST to this view from clients without a CSRF token
 @csrf_exempt
 def snippet_list(request):
-    """
-    List all code snippets, or create a new snippet.
-    """
-    # NOTE adding a many=True flag to the serializer arguments serializes querysets instead of model instances
+    # List all code snippets, or create a new snippet
+    # Adding a many=True flag to the serializer arguments serializes querysets instead of model instances
     if request.method == 'GET':
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
@@ -28,9 +76,7 @@ def snippet_list(request):
 
 @csrf_exempt
 def snippet_detail(request, pk):
-    """
-    Retrieve, update or delete a code snippet.
-    """
+    # Retrieve, update or delete a code snippet
     try:
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
@@ -54,3 +100,5 @@ def snippet_detail(request, pk):
     elif request.method == 'DELETE':
         snippet.delete()
         return HttpResponse(status=204)
+
+"""
