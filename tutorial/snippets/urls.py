d93db5cddd16cb3f1c840f42f4979e688d8ecee9
lgants <lgants@gmail.com>
2017-11-22 23:59:00 -0600

    switched to class-based views

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -3,9 +3,17 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+]
+
+"""
+# Deprecated when switching to class-based views
+urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)$', views.snippet_detail),
 ]
+"""
 
 # Added format suffixes that support URLs that explicitly refer to a given format - allows API to handle URLs such as http://example.com/api/items/4.json.
 urlpatterns = format_suffix_patterns(urlpatterns)
