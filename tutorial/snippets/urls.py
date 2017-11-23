e0b4dbeb87c1f1470298c8d995f7101f122539b3
lgants <lgants@gmail.com>
2017-11-22 23:50:33 -0600

    updated comment

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^snippets/(?P<pk>[0-9]+)$', views.snippet_detail),
 ]
 
-# Added format suffixes to give URLs that explicitly refer to a given format - allows API to handle URLs such as http://example.com/api/items/4.json.
+# Added format suffixes that support URLs that explicitly refer to a given format - allows API to handle URLs such as http://example.com/api/items/4.json.
 urlpatterns = format_suffix_patterns(urlpatterns)
